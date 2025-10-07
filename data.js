e11de81f40c32afb2525ebb545a2b1bd5ad3df9c
wtinghui <weetinghui@gmail.com>
2025-10-07 01:41:01 +0000

    Added status badge and add button. Also add logic to make rating appear if book has been read.

--- data.js
+++ data.js
@@ -12,7 +12,36 @@ async function loadData(){
 }
 
 function statusBadge(status){
-    if (status=="Want To Read"){
-        
+    if (status=="Completed"){
+        return "bg-success"
     }
+    if (status =="Reading"){
+        return "bg-primary"
+    }
+    if (status=="To read"){
+        return "bg-secondary"
+    }
+}
+
+function renderList(books){
+    cards.innerHTML = "";
+    books.forEach(function (book) {
+        cards.innerHTML+=`
+        <div class="card me-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
+                <p class="mb-2"> 
+                    <span class="badge rounded-pill ${statusBadge(book.status)}">${book.status}</span>
+                    <span> ${(book.status == "To read") ? '': `${book.ratings}/5`}</span>
+                </p>
+                <p class="card-text mb-2">Genre: ${book.genre} </p>
+            </div>
+            <div class="card-footer d-flex justify-content-around" >
+                <button class="btn btn-primary">Edit</button>
+                <button class = "btn btn-primary">Delete</button>
+            </div>
+        </div>
+        `
+    })
 }
