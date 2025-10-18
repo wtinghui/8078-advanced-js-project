bed909a46b1b4385eecbe4a2fb212e590eedb356
wtinghui <weetinghui@gmail.com>
2025-10-18 15:55:37 +0000

    Added functions - add new book, edit and delete

--- data.js
+++ data.js
@@ -1,47 +1,78 @@
 // create a reading list
 
-const jsonBinRoot="https://api.jsonbin.io/v3";
-const jsonBinID="68e0d616ae596e708f059c80";
+const jsonBinRoot = "https://api.jsonbin.io/v3";
+const jsonBinID = "68e0d616ae596e708f059c80";
 
-const url=jsonBinRoot+"/b/"+jsonBinID+"/latest";
-console.log(url);
+const url = jsonBinRoot + "/b/" + jsonBinID;
 
-async function loadData(){
-    const response=await axios.get(url);
+async function loadData() {
+    const response = await axios.get(url+"/latest");
     return response.data.record;
 }
 
-function statusBadge(status){
-    if (status=="Completed"){
+function statusBadge(status) {
+    if (status == "Completed") {
         return "bg-success"
     }
-    if (status =="Reading"){
+    if (status == "Reading") {
         return "bg-primary"
     }
-    if (status=="To read"){
+    if (status == "To read") {
         return "bg-secondary"
     }
 }
 
-function renderList(books){
+function renderList(books) {
     cards.innerHTML = "";
-    books.forEach(function (book) {
-        cards.innerHTML+=`
+    books.forEach(function (book,index) {
+        cards.innerHTML += `
         <div class="card me-2" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
                 <p class="mb-2"> 
                     <span class="badge rounded-pill ${statusBadge(book.status)}">${book.status}</span>
-                    <span> ${(book.status == "To read") ? '': `${book.ratings}/5`}</span>
+                    <span> ${(book.status == "Completed") ? `${book.ratings}/5` : ''}</span>
                 </p>
                 <p class="card-text mb-2">Genre: ${book.genre} </p>
             </div>
             <div class="card-footer d-flex justify-content-around" >
-                <button class="btn btn-primary">Edit</button>
-                <button class = "btn btn-primary">Delete</button>
+                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bookEntryModal" data-action="edit" data-index="${index}">Edit</button>
+                <button class = "btn btn-primary" data-action="delete" data-index="${index}">Delete</button>
             </div>
         </div>
         `
     })
+}
+function addBook(books, title, author, genre, status, ratings) {
+    const newBookEntry = {
+        "title": title,
+        "author": author,
+        "genre": genre,
+        "status": status,
+        "ratings": ratings
+    };
+
+    books.push(newBookEntry);
+    saveBooks(books);
+}
+
+function editBook(books,index,title,author,genre,status,ratings){
+    books[index]={
+        "title":title,
+        "author":author,
+        "genre":genre,
+        "status":status,
+        "ratings":ratings
+    };
+    saveBooks(books);
+}
+
+function deleteBook(books,index){
+    books.splice(index,1);
+    saveBooks(books);
+}
+
+async function saveBooks(books){
+    await axios.put(url,books)
 }
--- script.js
+++ script.js
@@ -1,8 +1,66 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const books = await loadData();
-    console.log(books);
-
     const cards = document.querySelector("#cards");
-    
+    const addBookEntry = document.querySelector("#addBookEntry");
+    const addBtn = document.querySelector("#addBtn");
+    const form = document.querySelector("#bookEntry");
+    const modalHeader = document.querySelector("#modalLabel");
+    const modalBtn = document.querySelector("#modalBtn");
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const genre = document.querySelector("#genre");
+    const status = document.querySelector("#status");
+    const ratings = document.querySelector("#ratings");
+
+
+    console.log(books);
     renderList(books);
+
+    addBookEntry.addEventListener("click", function () {
+        modalHeader.innerText = "Add new book";
+        modalBtn.innerText = "Add";
+        form.reset();
+
+        modalBtn.addEventListener("click",function(){
+            const titleValue = title?.value;
+            const authorValue = author?.value;
+            const genreValue = genre?.value;
+            const statusValue = status?.value;
+            const ratingsValue = (statusValue == "Completed") ? ratings?.value : "";
+
+            addBook(books, titleValue, authorValue, genreValue, statusValue, ratingsValue);
+            renderList(books);
+
+        })
+
+    })
+
+    cards.addEventListener("click", function (event) {
+        const index = parseInt(event.target.dataset.index);
+        if (event.target.dataset.action == "edit") {
+            const book = books[index];
+
+            form.reset();
+            modalHeader.innerText = "Edit book entry";
+            modalBtn.innerText = "Save";
+
+            title.value = book.title;
+            author.value = book.author;
+            genre.value = book.genre;
+            status.value = book.status;
+            ratings.value = book.ratings;
+
+            modalBtn.addEventListener("click",function(){
+                editBook(books,index,title.value,author.value,genre.value,status.value,ratings.value);
+                renderList(books);
+
+            })
+        }
+        if (event.target.dataset.action=="delete"){
+            deleteBook(books,index);
+            renderList(books);
+        }
+    });
+
+
 });
