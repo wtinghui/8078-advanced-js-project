8db7ede632b5f7a5d9c34b7ebee5192354253545
wtinghui <weetinghui@gmail.com>
2025-10-20 08:18:51 +0000

    Added interactive working bar. To return a value for ratings. Issue:Faulty database.

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const jsonBinID = "68e0d616ae596e708f059c80";
 const url = jsonBinRoot + "/b/" + jsonBinID;
 
 async function loadData() {
-    const response = await axios.get(url+"/latest");
+    const response = await axios.get(url + "/latest");
     return response.data.record;
 }
 
@@ -24,15 +24,30 @@ function statusBadge(status) {
 
 function renderList(books) {
     cards.innerHTML = "";
-    books.forEach(function (book,index) {
+    books.forEach(function (book, index) {
+
+        let starRatings="";        
+        if (book.status == "Completed") {
+            for (let i = 1; i < 6; i++) {
+                starRatings += `
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${(i <= book.ratings) ? "orange" : "lightGrey"}"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                `
+            }
+
+        }
+
         cards.innerHTML += `
         <div class="card me-2" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
                 <p class="mb-2"> 
-                    <span class="badge rounded-pill ${statusBadge(book.status)}">${book.status}</span>
-                    <span> ${(book.status == "Completed") ? `${book.ratings}/5` : ''}</span>
+                    <span class="badge rounded-pill ${statusBadge(book.status)} me-2">${book.status}</span>
+                    <span> ${(book.status == "Completed") ? starRatings : ''} </span> 
                 </p>
                 <p class="card-text mb-2">Genre: ${book.genre} </p>
             </div>
@@ -57,22 +72,22 @@ function addBook(books, title, author, genre, status, ratings) {
     saveBooks(books);
 }
 
-function editBook(books,index,title,author,genre,status,ratings){
-    books[index]={
-        "title":title,
-        "author":author,
-        "genre":genre,
-        "status":status,
-        "ratings":ratings
+function editBook(books, index, title, author, genre, status, ratings) {
+    books[index] = {
+        "title": title,
+        "author": author,
+        "genre": genre,
+        "status": status,
+        "ratings": ratings
     };
     saveBooks(books);
 }
 
-function deleteBook(books,index){
-    books.splice(index,1);
+function deleteBook(books, index) {
+    books.splice(index, 1);
     saveBooks(books);
 }
 
-async function saveBooks(books){
-    await axios.put(url,books)
+async function saveBooks(books) {
+    await axios.put(url, books)
 }
--- script.js
+++ script.js
@@ -11,9 +11,11 @@ document.addEventListener("DOMContentLoaded", async function () {
     const genre = document.querySelector("#genre");
     const status = document.querySelector("#status");
     const ratings = document.querySelector("#ratings");
+    const ratingsBar=document.querySelector("#ratingsBar");
+    const stars=document.querySelectorAll(".star");
 
+    console.log(stars);
 
-    console.log(books);
     renderList(books);
 
     addBookEntry.addEventListener("click", function () {
@@ -35,7 +37,78 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     })
 
-    cards.addEventListener("click", function (event) {
+    // ratingsBar.addEventListener("mouseenter",function(event){
+    //     event.stopPropagation();
+    //     console.log("mouseover")
+    //     event.target.firstElementChild.classList.add("hover")
+    // })
+
+    // ratingsBar.addEventListener("mouseout",function(event){
+    //     event.stopPropagation();
+    //     console.log("mouseleave");
+    //     event.target.firstElementChild.classList.remove("hover")
+    // })
+
+    stars.forEach(function(star,index){
+        star.addEventListener("mouseover",function(){
+            stars.forEach(function(s,i){
+                (i<=index)?s.classList.add("hover"):s.classList.add("not-hover")
+                console.log(s.id, s.classList)
+            })
+        })
+
+        star.addEventListener("mouseout",function(){
+            stars.forEach(function(s){
+               if (s.classList.contains("hover")){
+                    s.classList.remove("hover")
+                    }
+                if (s.classList.contains("not-hover")){
+                    s.classList.remove("not-hover")
+                    }
+                console.log(s.id, s.classList)
+            })
+        })
+
+        star.addEventListener("click",function(event){
+            stars.forEach(function(s,i){
+                if (s.classList.contains("selected")){
+                    s.classList.remove("selected")
+                    }
+                if (s.classList.contains("not-selected")){
+                    s.classList.remove("not-selected")
+                    }
+                (i<=index)?s.classList.add("selected"):s.classList.add("not-selected")
+                console.log(s.id, s.classList)
+            })
+        })
+    })
+
+    // stars.forEach(function(star,index){
+    //     // star.addEventListener("mouseout",function(event){
+    //     //     stars.forEach(function(s,i){
+    //     //         s.style.fill(i<=index)?"grey":"lightGrey"
+    //     //         (i<=index)?s.classList.remove("hover"):""
+    //     //     })
+    //     // })
+
+    //     star.addEventListener("mouseover",function(){
+    //         console.log(`mouse enter ${star.id}`)
+    //         stars.forEach(function(s,i){
+    //             console.log(s);
+    //             console.log(i, index, `${(i<=index)}`);
+    //             s.style.fill=(i<=index)?"grey":"lightGrey"
+    //         })
+    //     })
+    //     // s
+    //     star.addEventListener("click",function(event){
+    //         stars.forEach(function(s,i){
+    //             s.style.fill=(i<=index)?"orange":"lightGrey"
+    //         })
+    //     })
+    // })
+    
+    
+    cards.addEventListener("mouseover", function (event) {
         const index = parseInt(event.target.dataset.index);
         if (event.target.dataset.action == "edit") {
             const book = books[index];
@@ -61,6 +134,8 @@ document.addEventListener("DOMContentLoaded", async function () {
             renderList(books);
         }
     });
+
+
 
 
 });
